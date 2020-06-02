2a046d1fdfe68c363eb533b506b3d0bd2f638929
ST_Kelly.Liu 劉姿君 <Kelly.Liu@solartninc.com>
2020-06-02 10:14:23 +0800

    fix: apple_download.py

--- apple_download.py
+++ apple_download.py
@@ -1,17 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
 import time
 import os
 import sys
+import csv
+from bs4 import BeautifulSoup
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import csv
 
-url_apple_download = 'http://www.ggkjplus.com/admin/#/login'
-chromedriver = '/usr/local/share/chromedriver'
 
-account_apple = os.environ.get('APPLE_ACCOUNT')
-password_apple = os.environ.get('APPLE_PASSWORD')
+# In[6]:
+
+
+CHROMEDRIVER_PATH = './tools/chromedriver'
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
@@ -20,21 +29,29 @@ options.add_argument('--disable-gpu')
 options.add_argument('blink-settings=imagesEnabled=false')
 options.add_argument('--enable-features=OverlayScrollbar')
 
-driver = webdriver.Chrome(executable_path=chromedriver, options=options)
-driver.implicitly_wait(60)
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=options)
 
-driver.get(url_apple_download);
-driver.find_element_by_xpath('/html/body/div/div[2]/div[2]/form/div[2]/div/div/input').send_keys(account_apple)
-driver.find_element_by_xpath('/html/body/div/div[2]/div[2]/form/div[3]/div/div/input').send_keys(password_apple)
-driver.find_element_by_xpath('/html/body/div/div[2]/div[2]/form/button/span').click()
+driver.get("https://www.ggkjplus.com/admin/#/login")
+time.sleep(2)
+driver.find_element(By.NAME, "mobile").click()
+driver.find_element(By.NAME, "mobile").send_keys("13053948149")
+driver.find_element(By.NAME, "pwd").click()
+driver.find_element(By.NAME, "pwd").send_keys("fHZPBE9fpKnup4r")
+driver.find_element(By.CSS_SELECTOR, ".over-btn > span").click()
+time.sleep(5)
+soup = BeautifulSoup(driver.page_source,"html.parser")
+driver.close()
 
-time.sleep(3)
-driver.get('https://www.ggkjplus.com/admin/#/webpackage')
-time.sleep(3)
-result = driver.find_element_by_xpath('/html/body/div/div[3]/div[2]/section/div/div[1]/ul/li[1]/span/div/span').text
-#print('balance: ',result)
+result = int(soup.find_all("div",class_="iCountUp num")[1].text.replace(",",""))
+print("balance: ",result)
+
 with open('output.csv', 'w', newline='') as csvfile:
-  writer = csv.writer(csvfile)
-  writer.writerow([result])
+    writer = csv.writer(csvfile)
+    writer.writerow([result])
 
-driver.quit()
+
+# In[ ]:
+
+
+
+
